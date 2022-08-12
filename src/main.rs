7be096e814186747420b150fc6553b125ddb44b0
loloof64 <laurent.bernabe@gmail.com>
2022-08-12 15:43:18 +0200

    Added a surface with a grey background

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use gtk::DrawingArea;
 use gtk::prelude::*;
 use gtk::{Application, ApplicationWindow};
 
@@ -9,10 +10,19 @@ fn main() {
     app.connect_activate(|app| {
         let win = ApplicationWindow::builder()
             .application(app)
-            .default_width(320)
-            .default_height(200)
+            .default_width(400)
+            .default_height(400)
             .title("Basic Chess Endgames")
             .build();
+
+        let drawing_area = DrawingArea::new();
+        drawing_area.connect_draw(|_drawing_area, cx| {
+            cx.set_source_rgb(0.3, 0.3, 0.3);
+            cx.rectangle(0.0, 0.0, 400.0, 400.0);
+            cx.fill().expect("Failed to fill context");
+            Inhibit(false)
+        });
+        win.add(&drawing_area);
 
         win.show_all();
     });
