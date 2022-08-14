fcbe1372d8fa5e38a55f2c6aac0e5df2808b8c30
loloof64 <laurent.bernabe@gmail.com>
2022-08-14 22:17:10 +0200

    Handling pieces memories in a dedicated struct.

--- README.md
+++ README.md
@@ -4,4 +4,4 @@ Generate a chess endgame and play it against the computer.
 
 ## Credits
 
-Chess piece vector has been downloaded from [Wikimedia Commons](https://commons.wikimedia.org/wiki/Category:SVG_chess_pieces) and designed by Cburnett.
+Chess pieces vectors have been downloaded from [Wikimedia Commons](https://commons.wikimedia.org/wiki/Category:SVG_chess_pieces) and designed by Cburnett.
--- src/widgets/chessboard/mod.rs
+++ src/widgets/chessboard/mod.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+
 use cairo::Context;
 use gdk_pixbuf::Pixbuf;
 use gio::MemoryInputStream;
@@ -8,26 +10,22 @@ use gtk::DrawingArea;
 #[derive(Clone)]
 pub struct ChessBoard {
     size: i32,
+    images: PiecesImages,
 }
 
 impl ChessBoard {
     pub fn new(size: i32) -> Self {
-        Self { size }
+        let images = PiecesImages::new(size);
+        Self { size, images }
     }
 
     pub fn view(&self) -> DrawingArea {
-        let image_data = include_bytes!("./vectors/Chess_blt45.svg");
-        let image_data = Bytes::from(image_data);
-        let image_stream = MemoryInputStream::from_bytes(&image_data);
-        let pixbuf =
-            Pixbuf::from_stream_at_scale(&image_stream, 140, 140, true, None::<&gio::Cancellable>)
-                .expect("Failed to interpret image.");
-
         let board_clone = self.clone();
         let drawing_area = DrawingArea::new();
         drawing_area.set_size_request(self.size, self.size);
         {
             drawing_area.connect_draw(move |_drawing_area, cx| {
+                let pixbuf = &board_clone.images.pixbufs[&'N'];
                 Painter::draw_background(cx);
                 Painter::draw_piece(cx, &pixbuf);
                 Painter::draw_cells(cx, &board_clone);
@@ -67,5 +65,76 @@ impl Painter {
         );
         cx.rectangle(10.0, 20.0, cells_size, cells_size);
         cx.fill().unwrap();
+    }
+}
+
+#[derive(Clone)]
+struct PiecesImages {
+    streams: HashMap<char, MemoryInputStream>,
+    pixbufs: HashMap<char, Pixbuf>,
+}
+
+impl PiecesImages {
+    pub fn new(size: i32) -> Self {
+        let streams = PiecesImages::build_streams();
+        let pixbufs = PiecesImages::build_pixbufs(&streams, size);
+
+        Self { streams, pixbufs }
+    }
+
+    fn build_streams() -> HashMap<char, MemoryInputStream> {
+        let mut result = HashMap::new();
+        let pieces_types = vec!['P', 'N', 'B', 'R', 'Q', 'K', 'p', 'n', 'b', 'r', 'q', 'k'];
+        let svg_defs: Vec<&[u8]> = vec![
+            include_bytes!("./vectors/Chess_plt45.svg"),
+            include_bytes!("./vectors/Chess_nlt45.svg"),
+            include_bytes!("./vectors/Chess_blt45.svg"),
+            include_bytes!("./vectors/Chess_rlt45.svg"),
+            include_bytes!("./vectors/Chess_qlt45.svg"),
+            include_bytes!("./vectors/Chess_klt45.svg"),
+            include_bytes!("./vectors/Chess_pdt45.svg"),
+            include_bytes!("./vectors/Chess_ndt45.svg"),
+            include_bytes!("./vectors/Chess_bdt45.svg"),
+            include_bytes!("./vectors/Chess_rdt45.svg"),
+            include_bytes!("./vectors/Chess_qdt45.svg"),
+            include_bytes!("./vectors/Chess_kdt45.svg"),
+        ];
+        let pieces_refs: Vec<(_, _)> = pieces_types.iter().zip(svg_defs.iter()).collect();
+
+        for (kind, data) in pieces_refs.iter() {
+            let kind = **kind;
+            let image_data = **data;
+
+            let image_data = Bytes::from(image_data);
+            let image_stream = MemoryInputStream::from_bytes(&image_data);
+
+            result.insert(kind, image_stream);
+        }
+
+        result
+    }
+
+    fn build_pixbufs(
+        streams: &HashMap<char, MemoryInputStream>,
+        size: i32,
+    ) -> HashMap<char, Pixbuf> {
+        let mut result = HashMap::new();
+
+        for kind in streams.keys() {
+            let image_stream = &streams[kind];
+
+            let pixbuf = Pixbuf::from_stream_at_scale(
+                image_stream,
+                size,
+                size,
+                true,
+                None::<&gio::Cancellable>,
+            )
+            .expect("Failed to interpret image.");
+
+            result.insert(*kind, pixbuf);
+        }
+
+        result
     }
 }
